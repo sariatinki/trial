61ec06f3aa78d13eaa88e918bc38e3fa2aab8477
sariatinki <46630559+sariatinki@users.noreply.github.com>
2019-06-25 09:41:52 -0400

    Update 011prime.py

--- 011prime.py
+++ 011prime.py
@@ -2,7 +2,7 @@ digit = int(input("Enter a Number:"))
 for num in range(2, digit):
     if digit%num == 0:
         result = "not prime"
-        break
+        
     else:
         result = "prime"
 print(result)
